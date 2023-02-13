77e4d370bac37c2a2af45551a7f2e0eda32053c3
Anjisnu Roy <royanjisnu78@gmail.com>
2023-02-13 23:19:16 +0530

    Add files via upload

---
+++ 1.Student information.cpp
@@ -0,0 +1,59 @@
+// Q.no1==> Write a program to show student information using class object.
+
+#include<iostream>
+
+using namespace std;
+
+class obj{
+	public:
+		int id;
+		int marks;
+		string name;
+		
+		void getname(){
+			cout << "Enter the name of the student: ";
+			cin >> name;
+		}
+		
+		void getid(){
+			cout << "Enter the id of the student: ";
+			cin >> id;
+		}
+		
+		void getmarks(){
+			cout << "Enter the marks of the student: ";
+			cin >> marks;
+		}
+};
+
+
+int main(){
+	obj object;
+	int no;
+	int num;
+	int sum=0;
+	cout << "Enter the no of student:";
+	cin >> no;
+	
+	for(int i=0;i<no;i++){
+			cout << "Details of " << i+1 << "th student.\n";
+		object.getname();
+		object.getmarks();
+	num=	object.getid();
+		sum=sum+num;
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+

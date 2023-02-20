3ebd05335bf7fbc871d0646971d215b00abe44c8
Anjisnu Roy <royanjisnu78@gmail.com>
2023-02-20 20:30:29 +0530

    Update 1.Student information.cpp

--- 1.Student information.cpp
+++ 1.Student information.cpp
@@ -1,47 +1,53 @@
 // Q.no1==> Write a program to show student information using class object.
 
 #include<iostream>
-
 using namespace std;
+class student {
+    int roll;
+    float marks;
+    public:
+    	 
+    void input(){
+    
+	cout<<"\n Enter Roll no";	
+    cin >> roll;
+    cout<<" your Roll no:" << roll;
+    
+    cout <<"\nEnter your Marks>";
+    cin >> marks;
+    cout <<" Marks>"<<marks;;
+    
 
-class obj{
-	public:
-		int id;
-		int marks;
-		string name;
+}
+
+	int  display(){
 		
-		void getname(){
-			cout << "Enter the name of the student: ";
-			cin >> name;
-		}
-		
-		void getid(){
-			cout << "Enter the id of the student: ";
-			cin >> id;
-		}
-		
-		void getmarks(){
-			cout << "Enter the marks of the student: ";
-			cin >> marks;
-		}
+    cout << "\nRoll no."<< roll << "\nMarks" <<marks;
+    return 0;
+}
+
+
 };
 
 
 int main(){
-	obj object;
-	int no;
-	int num;
-	int sum=0;
-	cout << "Enter the no of student:";
-	cin >> no;
+	int i,n;
+    student s[10];
+    
+    cout << "Enter No of student you have:" <<endl;
+    cin >> n;
+    for (i=1;i<=n;i++) {
 	
-	for(int i=0;i<no;i++){
-			cout << "\nDetails of " << i+1 << "th student.\n";
-		object.getname();
-		object.getmarks();
-		object.getid();
-		
+           s[i].input();
+           
 	}
+	cout << "\n	Your entered data is:-" << endl;
+	for (i=1;i<=n;i++) {
+	
+           
+           s[i].display(); 
+	}
+    
+return 0;
 }
-
 
